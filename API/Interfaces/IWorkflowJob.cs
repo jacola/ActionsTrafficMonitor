c7b8fc404e78bd3d037894094971ce07fdc9f0a9
Jacques <941214+jacola@users.noreply.github.com>
2022-11-29 17:07:33 +0900

    Rename webhook payload object

--- API/Interfaces/IWorkflowJob.cs
+++ API/Interfaces/IWorkflowJob.cs
@@ -2,9 +2,10 @@
 
 namespace API.Interfaces
 {
-    public class WorkflowJob
+    public class IWorkflowJob
     {
         public long Id { get; set; }
+        [JsonPropertyName("run_id")] public long RunId { get; set; }
         public string Name { get; set; }
         [JsonPropertyName("started_at")] public DateTime StartedAt { get; set; }
         [JsonPropertyName("completed_at")] public DateTime? CompletedAt { get; set; }
