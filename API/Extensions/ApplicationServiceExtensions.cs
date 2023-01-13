b4ae40fe73860a07a6f0476efd172a4bb07b01a0
Jacques Lalancette <941214+jacola@users.noreply.github.com>
2023-01-13 10:31:48 +0900

    Swaitch to postgres

--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -12,13 +12,42 @@ namespace API.Extensions
         public static IServiceCollection AddApplicationServices(this IServiceCollection services,
             IConfiguration config)
         {
-            services.AddSwaggerGen(c =>
+            // There's probably a better way to set this up. This follows the spirit of this
+            // suggestion from 2019:
+            // https://github.com/dotnet/aspnetcore/issues/9337#issuecomment-539859667
+            using var loggerFactory = LoggerFactory.Create(builder =>
             {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebAPIv5", Version = "v1" });
+                builder.AddConsole();
+                builder.AddEventSourceLogger();
             });
+            var logger = loggerFactory.CreateLogger("ApplicationServiceExtensions");
+
+            services.AddSwaggerGen(c =>
+                {
+                    c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebAPIv5", Version = "v1" });
+                });
             services.AddDbContext<DataContext>(opt =>
             {
-                opt.UseSqlite(config.GetConnectionString("DefaultConnection"));
+                string pgUsername = Environment.GetEnvironmentVariable("PG_USERNAME");
+                string pgPassword = Environment.GetEnvironmentVariable("PG_PASSWORD");
+                string pgServer = Environment.GetEnvironmentVariable("PG_SERVER");
+                string pgDatabase = Environment.GetEnvironmentVariable("PG_DATABASE");
+
+                if (pgUsername == null) logger.LogInformation("PG_USERNAME environment variable not set");
+                if (pgPassword == null) logger.LogInformation("PG_PASSWORD environment variable not set");
+                if (pgServer == null) logger.LogInformation("PG_SERVER environment variable not set");
+                if (pgDatabase == null) logger.LogInformation("PG_DATABASE environment variable not set");
+
+                bool UseEnvVars = pgUsername != null && pgPassword != null && pgServer != null && pgDatabase != null;
+
+                if (!UseEnvVars)
+                    logger.LogWarning("Using development database connection string.");
+
+                string connectionString = UseEnvVars
+                    ? $"Host={pgServer};Database={pgDatabase};Username={pgUsername};Password={pgPassword}"
+                    : config.GetConnectionString("DefaultConnection");
+
+                opt.UseNpgsql(connectionString);
             });
 
             // For development
